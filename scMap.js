78dff58c0bec54004eb1258dad967d8684738f7d
John Reading <john@resonancemultimedia.com>
2012-02-10 12:28:05 -0500

    "offering jQuery version as separate file"

--- scMap.js
+++ scMap.js
@@ -1,20 +1,22 @@
 var siteCatalyst = {
 	init: function(el) {
 		if (typeof scMap != "undefined") {
-			$el = !el ? $('body') : $(el);
-			$('*[data-sc-item]:not(form)',$el).each($.proxy(function(idx,el) {
-				this.attachLinkEvents(el);
-			},this));
-			$('form[data-sc-item]',$el).each($.proxy(function(idx,el) {
-				this.attachFormSubmit($(el));
-			},this));
+			$el = !el ? document.querySelector('body') : document.querySelector(el);
+
+			[].forEach.call($el.querySelectorAll('*[data-sc-item]:not(form)'), function(el) {
+				siteCatalyst.attachLinkEvents(el);
+			});
+			
+			[].forEach.call($el.querySelectorAll('form[data-sc-item]'), function(el) {
+				siteCatalyst.attachFormSubmit(el);
+			});
 		}
 	},
 	attachLinkEvents: function(el) {
-		ev = $(el).data('sc-event') ? $(el).data('sc-event') : 'click';
-		$(el).bind(ev,function(e) {
-			link = $(el).data('sc-url') ? $(el).data('sc-url') : e.target;
-			mapItem = $(el).data('sc-item');
+		ev = el.dataset.scEvent ? el.dataset.scEvent : 'click';
+		el.addEventListener(ev,function(e) {
+			link = el.dataset.scUrl ? el.dataset.scUrl : e.target;
+			mapItem = el.dataset.scItem;
 			for (prop in scMap[mapItem]){
 				s[prop] = siteCatalyst.replaceWildCards(scMap[mapItem][prop], el);
 			}
@@ -22,12 +24,13 @@ var siteCatalyst = {
 		});
 	},
 	attachFormSubmit: function(el) {
-		$(el).bind('submit',function(e) {
-			link = $(el).attr('action');
-			mapItem = $(el).data('sc-item');
+		el.addEventListener('submit',function(e) {
+			link = el.getAttribute('action');
+			mapItem = el.dataset.scItem;
 			for (prop in scMap[mapItem]) {
 				s[prop] = siteCatalyst.replaceWildCards(scMap[mapItem][prop], el);
 			}
+			
 			if (scMap[mapItem]) s.tl(link,scMap[mapItem]["type"],s['linkTrackName']);
 		});
 	},
@@ -35,12 +38,12 @@ var siteCatalyst = {
 		pattern = new RegExp(/{(.*?)}/g); 
 		if (pattern.test(str)) { 
 			wildcards = str.match(pattern);
-			for (i=0; i < wildcards.length; i++){
+			for (var i=0; i < wildcards.length; i++){
 				atr = (/{(.*?)}/).exec(wildcards[i])[1];
-				if ($(el).get(0).nodeName.toLowerCase() != 'form') {
-					str = str.replace(wildcards[i],$(el).attr(atr));
+				if (el.nodeName.toLowerCase() != 'form') {
+					str = str.replace(wildcards[i],el.getAttribute(atr));
 				} else {
-					str = str.replace('{'+ atr +'}',$('*[name="'+ atr +'"]',el).val());
+					str = str.replace('{'+ atr +'}',el.querySelector('*[name="'+ atr +'"]').value);
 				}
 			}
 		}
